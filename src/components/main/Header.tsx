5154d6c6c384ffd95b997aa525d6a72b6a04d072
Emi2710 <deadourfreedom@gmail.com>
2023-06-09 16:37:22 +0300

    Update Header.tsx

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({}: Props) {
         <div className='mt-12 max-w-md lg:max-w-xl lg:pt-12'>
             <h1 className='tracking-wider text-4xl lg:text-6xl p-5 '>Школа хафизов для девушек в грозном</h1>
             <p className='roboto leading-relaxed opacity-80 px-5 text-xs lg:text-sm pb-5'>У вас есть возможность получить знания у квалифицированных преподавателей в области изучения Священного писания и тем самым стать носителями Корана.</p>
-            <Link href="about-school" className='p-5'><button className='tracking-widest light-golden-bg px-5 py-1'>Узнать подробнее </button></Link>
+            <Link href="o-shkole" className='p-5'><button className='tracking-widest light-golden-bg px-5 py-1'>Узнать подробнее </button></Link>
         </div>
         <div>
             <Image src={hero} alt="Школа хафизов для девушек в грозном" className="p-5 mt-5 max-w-sm lg:max-w-md width-100"/>
